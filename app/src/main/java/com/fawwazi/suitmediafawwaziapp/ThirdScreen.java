a69b594aaec15f0091c562affd49119090e75437
Naufal Fawwazi <faw10zi@gmail.com>
2023-01-19 23:59:58 +0700

    Create UserData and UserResponse class

--- app/src/main/java/com/fawwazi/suitmediafawwaziapp/ThirdScreen.java
+++ app/src/main/java/com/fawwazi/suitmediafawwaziapp/ThirdScreen.java
@@ -3,12 +3,23 @@ package com.fawwazi.suitmediafawwaziapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class ThirdScreen extends AppCompatActivity {
+    private ImageView ivBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third_screen);
+
+        ivBack = findViewById(R.id.iv_back);
+        ivBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/fawwazi/suitmediafawwaziapp/UserResponse.java
@@ -0,0 +1,48 @@
+package com.fawwazi.suitmediafawwaziapp;
+
+import java.util.List;
+
+public class UserResponse {
+    int page, per_page, total, total_pages;
+    List<UserData> data;
+
+    public int getPage() {
+        return page;
+    }
+
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public int getPer_page() {
+        return per_page;
+    }
+
+    public void setPer_page(int per_page) {
+        this.per_page = per_page;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
+    public int getTotal_pages() {
+        return total_pages;
+    }
+
+    public void setTotal_pages(int total_pages) {
+        this.total_pages = total_pages;
+    }
+
+    public List<UserData> getData() {
+        return data;
+    }
+
+    public void setData(List<UserData> data) {
+        this.data = data;
+    }
+}
