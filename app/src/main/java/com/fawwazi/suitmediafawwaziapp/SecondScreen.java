8c7dcfca828bd7136527d1bee5644d4034012175
Naufal Fawwazi <faw10zi@gmail.com>
2023-01-19 23:32:40 +0700

    Finished Second Screen Java Class

--- app/src/main/java/com/fawwazi/suitmediafawwaziapp/SecondScreen.java
+++ app/src/main/java/com/fawwazi/suitmediafawwaziapp/SecondScreen.java
@@ -2,13 +2,40 @@ package com.fawwazi.suitmediafawwaziapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class SecondScreen extends AppCompatActivity {
+public class SecondScreen extends AppCompatActivity implements View.OnClickListener {
+    private TextView tvName, tvSelectedName;
+    private Button btnChoose;
+    private ImageView ivBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_screen);
+
+        tvName = findViewById(R.id.tv_name);
+        tvSelectedName = findViewById(R.id.tv_selectedname);
+        btnChoose = findViewById(R.id.btn_choose);
+        ivBack = findViewById(R.id.iv_back);
+
+        tvName.setText(getIntent().getStringExtra("NAME"));
+        btnChoose.setOnClickListener(this);
+        ivBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_choose) {
+            Intent i = new Intent(this, ThirdScreen.class);
+            startActivity(i);
+        } else if (v.getId() == R.id.iv_back) {
+            finish();
+        }
     }
 }
